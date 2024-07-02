64deb57ff7529242b30fce955746718343f0b4b2
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-01 22:03:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// if ((x === y || a === b) && c === d) {
+//     g = h;
+// }
+//  else {
+//      e = f;
+//  }
+ 
+// var city0 = "Turkey";
+// var city1 = "Iran";
+// var city2 = "Saudia Arab";
+// var city3 = "Africa";
+// var city4 = "America";
+// var city5 = "China";
+
+// alert("welcome to" + " " + city3);
+
+// console.log(city1);
+
+
+
+// var pets = [];
+// pets[0] = "dog";
+// pets[1] = "cat"
+// pets[2] = "bird";
+// pets[3] = "lizard";
+// pets[6] = "snake";
+
+
+//  pets[3] = "lizard";
+// pets[4] = "fish"; 
+// pets[5] = "gerbil";
+//  pets[6] = "snake"
+
+//  pets.pop();
+//  pets.push("fish", "ferret");
+
+
+
+
+
+
+
